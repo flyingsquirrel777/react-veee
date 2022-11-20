f5249963c5168d887840671cf9e1f357b9aeae86
Minghao Chen <qiwen_li@brown.edu>
2022-11-19 23:16:13 -0500

    yrdy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
+import groceryData from "./assets/grocery-data.json"
+import GrocercyItem from './components/GroceryItem';
 function App() {
+  const  [cartItems, setCartItems] = useState([])
+  // const  [price, setPrice] = useState(0)
+
+  function addToCart(item){
+    setCartItems([...cartItems, item])
+  }
+
+  function CalculateTotal(){
+    let total = 0;
+    for(let i =0; i < cartItems.length; i++){
+      total += cartItems[i].price
+    }
+    return total
+    
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hello world</p>
+      <h1>Veee Groceries</h1> 
+      <div className='Content'>
+        <div className='GroceryContainer'>
+          {
+            groceryData.map((item)=>(
+              <GrocercyItem item = {item} addToCart ={addToCart}></GrocercyItem>
+            ))
+          }
+
+        </div>
+        <div className="Cart">
+          <h2>Cart</h2>
+          {cartItems.map((item,index)=>(
+            <p> {item.name}</p>
+          ))}
+          
+          <h3>Total Price: {CalculateTotal()}</h3>
+        </div>
+      </div>
+      
+
     </div>
   );
+ 
 }
 
 export default App;
