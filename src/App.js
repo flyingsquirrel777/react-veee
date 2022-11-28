b1f5f895e2b5da5670cd5f560fa7f2dcf07fe593
Minghao Chen <qiwen_li@brown.edu>
2022-11-28 17:30:01 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -2,69 +2,174 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 import groceryData from "./assets/grocery-data.json"
+import typeData from "./assets/type-data.json"
 import GrocercyItem from './components/GroceryItem';
-import NavBar from './components/NavBar';
-
-
 function App() {
-  const  [cartItems, setCartItems] = useState([]);
-  const  [type, setType] = useState("All");
-
-//  function type_setter(input_type){
-//   setType(input_type)
-//  }
-
- function selectFilterType(input_type){
-  setType(input_type)
- }
-
- function matchesFilterType(item){
-  if (type === "All"){
-    return true
-  }else if (type === item.types){
-    return true
-  }else{
-    return false
-  }
-
-
- }
- const filteredData = groceryData.filter(item=>{
-  return matchesFilterType(item)
-})
-
-
- 
-
-
+  const  [cartItems, setCartItems] = useState([])
+  const  [typeDatas, setTypeDatas] = useState(typeData)
+  const  [groceryDatas, setGroceryDatas] = useState(groceryData.map(item=>{
+	  item.num=0;
+	  item.isShow=true
+	  return item
+  }))
   // const  [price, setPrice] = useState(0)
 
   function addToCart(item){
-    setCartItems([...cartItems, item])
+	  
+	  let list=groceryDatas.map(gitem=>{
+		  return {
+			  ...gitem,
+			  num:gitem.name === item.name?1:gitem.num
+		  }
+	  })
+	  console.log('groceryDatas',list)
+	setGroceryDatas([...list])
+    setCartItems([...cartItems, {
+		...item,
+		num:1
+	}])
   }
-
+function subNum(item){
+	let list=groceryDatas.map(gitem=>{
+			  return {
+				  ...gitem,
+				  num:gitem.name===item.name?(item.num-1):gitem.num
+			  }
+	})
+	  console.log('groceryDatas',list)
+	setGroceryDatas([...list])
+	let cartItemsList=[];
+	cartItems.forEach(sitem=>{
+		if(sitem.name===item.name){
+			if(sitem.num>1){
+			cartItemsList.push({
+				...item,
+				num:sitem.num-1
+			})
+			}
+		}else{
+			
+			cartItemsList.push(sitem)
+		}
+	})
+	setCartItems([...cartItemsList])
+}
+function addNum(item){
+	let list=groceryDatas.map(gitem=>{
+			  return {
+				  ...gitem,
+				  num:gitem.name===item.name?(item.num+1):gitem.num
+			  }
+	})
+	setGroceryDatas([...list]);
+	let cartItemsList=[];
+	cartItems.forEach(sitem=>{
+		if(sitem.name===item.name){
+			cartItemsList.push({
+				...item,
+				num:sitem.num+1
+			})
+		}else{
+			cartItemsList.push(sitem)
+		}
+	})
+	setCartItems([...cartItemsList])
+}
+function showlist(){
+	return groceryDatas.filter(item=>item.isShow)
+}
   function CalculateTotal(){
     let total = 0;
     for(let i =0; i < cartItems.length; i++){
-      total += cartItems[i].price
+      total += cartItems[i].price*cartItems[i].num
     }
-    return total
+    return total.toFixed(2)
     
   }
+  // sort by ...
+  function compare(property){
+    return function(obj1,obj2){
+        var value1 = obj1[property];
+        var value2 = obj2[property];
+        return value1 - value2;     // sort
+    }
+   }
+  function handleChange(event,name,item){
+      let temp = [],typeArr=[];
+      item.checked = event.target.checked;
+      //filter target items
+      typeDatas.map(item=>{
+        item.category.map(citem=>{
+          if(citem.checked){
+            typeArr.push(citem.type)
+          }
+        })
+      })
+	temp = groceryDatas.map(item=>{
+		item.isShow=true;
+		return item
+	})
+      console.log('%c [ typeArr ]-46', 'font-size:13px; background:pink; color:#bf2c9f;', typeArr)
+      if(typeArr.includes('price')){
+         temp.sort(compare('price')) ; 
+      }else{
+		  let list=[];
+		  groceryData.forEach(item=>{
+			  let info=groceryDatas.find(sitem=>sitem.name===item.name);
+			  list.push(info)
+		  })
+		  temp = [...list]
+	  }
+	  if(typeArr.includes('meat')||typeArr.includes('vegetables')||typeArr.includes('fruits')){
+		temp=temp.map(gitem=>{
+			return {
+				...gitem,
+				isShow:typeArr.includes(gitem.Types)
+			}
+		})
+		 console.log('temp2',temp)
+	 }
+	 
+	 if(typeArr.includes('newArrival')||typeArr.includes('onSale')){
+		temp=temp.map(gitem=>{
+			return {
+				...gitem,
+				isShow:gitem.isShow?typeArr.includes(gitem.special):false
+			}
+		});
+	 }
+	 
+	 console.log('temp3',temp)
+      setGroceryDatas([...temp])
+  }
+
 
   
   return (
     <div className="App">
       <h1>Veee Groceries</h1> 
       <div className='Content'>
-        {/* <Nav onSelect={selectFilterType}>
-          <Nav.Item><Nav.Link eventKey="All"></Nav.Link></Nav.Item>
-        </Nav> */}
-        <NavBar selectFilterType = {selectFilterType}></NavBar>
+        <div className='siderBar'>
+          {
+            typeDatas.map((item)=>(
+              <div key={item.name}>
+                <p className="Catagories">{item.name}</p>
+                  {
+                    item.category.map((sitem)=>(
+                      <div key={item.type}>
+                        <input type='checkbox' id={sitem.type}  onChange={(event)=>handleChange(event,item.name,sitem)}></input>
+                        <label htmlFor={sitem.type}>{sitem.desc}</label>
+                      </div>
+                    ))
+                  }
+              </div>
+            ))
+          }
+        </div>
         <div className='GroceryContainer'>
           {
-            filteredData.map((item)=>(
-              <GrocercyItem item = {item} addToCart ={addToCart}></GrocercyItem>
+            showlist().map((item)=>(
+              <GrocercyItem item = {item} addToCart ={addToCart} subNum={subNum} addNum={addNum}></GrocercyItem>
             ))
           }
 
@@ -72,10 +177,23 @@ function App() {
         <div className="Cart">
           <h2>Cart</h2>
           {cartItems.map((item,index)=>(
-            <p> {item.name}</p>
+            <div className='cartList'>
+            <div className="info">
+                    <div><img src={item.image}></img></div>
+                    <div>
+                      <p> {item.name}</p>
+                      <p>$ {item.price}</p>
+                      <div>
+                        <button onClick={() =>subNum(item)}>-</button>
+                        <input type="text" className='num_box' value={item.num} />
+                        <button onClick={() => addNum(item)}>+</button>
+                      </div>
+                    </div>
+              </div>
+            </div>
           ))}
           
-          <h3>Total Price: {CalculateTotal()}</h3>
+          <h4>Total Price: $ {CalculateTotal()}</h4>
         </div>
       </div>
       
--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -3,18 +3,25 @@ import { useState } from "react";
 // TODO: create a component that displays a single bakery item
 function GrocercyItem(props){
     const item = props.item
-    const price = item.price
-    // const type = item.type
+    
     // const name = props.item.name
     return (
+
         <div className="GrocercyItem">
             <img src={item.image}></img>
-            <p>{item.name}</p>
+            <p className="itemName">{item.name}</p>
             <p>{item.description}</p>
-            <p>${item.price}</p>
-            <p>type: {item.types}</p>
-            <button onClick={()=>{props.addToCart(item)}}>Add to cart</button>
-
+            <p> type: {item.Types}   ${item.price} </p>
+            <p className="promo">{item.special}</p>
+			{item.num>=1?
+			(
+			<>
+			<button onClick={() =>props.subNum(item)}>-</button>
+            <input type="text" className='num_box' value={item.num} />
+            <button onClick={() => props.addNum(item)}>+</button>
+			</>):(<button className="addtocart" onClick={()=>{props.addToCart(item)}}>Add to cart</button>)}
+          
+		
         </div>
     );
 }
